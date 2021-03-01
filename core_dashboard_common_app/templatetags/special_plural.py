5700847d060a989e8b578bae5dd393466266e4d5
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2021-02-28 22:30:51 -0500

    fix: temp fix to deal with plural of objects from enum
    Change-Id: Ia49592cdbb1acfe57dcec62c35f916a3556bb251

---
+++ core_dashboard_common_app/templatetags/special_plural.py
@@ -0,0 +1,15 @@
+""" Core dashboard tag to pluralize labels
+FIXME: temporary solution, need to update the template and constant system to deal with singular/plural
+"""
+
+
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name="special_plural")
+def special_case_plural(value):
+    if value.endswith("y"):
+        return value.replace("y", "ies")
+    return f"{value}s"
