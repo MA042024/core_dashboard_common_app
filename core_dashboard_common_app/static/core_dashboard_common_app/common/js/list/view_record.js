0238867dbf363ef765a4ccdf213f41e071914fe3
hamza <hnb8@nsit.gov>
2022-07-21 10:18:06 -0400

    feat: add spinners to dashboard buttons
    Change-Id: Iceba02e7115a861e3c92b18d031e781bb6644536

--- core_dashboard_common_app/static/core_dashboard_common_app/common/js/list/view_record.js
+++ core_dashboard_common_app/static/core_dashboard_common_app/common/js/list/view_record.js
@@ -5,7 +5,11 @@
 openViewRecord = function() {
     var $registryRow = $(this).closest('tr');
     var objectID = $registryRow.attr("objectid");
+    var icon = $(this).find( "i" ).attr("class");
+    // Show loading spinner
+    showSpinner($(this).find("i"))
     window.location = viewRecordUrl + '?id=' + objectID;
+    hideSpinner($(this).find("i"), icon)
 };
 
 $(".view-record-btn").on('click', openViewRecord);
