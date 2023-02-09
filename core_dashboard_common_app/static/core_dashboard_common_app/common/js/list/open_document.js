592c8c196ec0e611a7d44aab9877fe5a4c0f6baa
hamza <hnb8@nsit.gov>
2023-02-09 09:47:14 -0500

    fix: hide spinner on failure
    Change-Id: I095f50b83aeee21382f256a398316d23e7fc4d65

--- core_dashboard_common_app/static/core_dashboard_common_app/common/js/list/open_document.js
+++ core_dashboard_common_app/static/core_dashboard_common_app/common/js/list/open_document.js
@@ -5,22 +5,28 @@
 $(".open-template-btn").on('click',function(){
     var $registryRow = $(this).closest('tr');
     var objectID = $registryRow.attr("object-id");
+    var icon = $(this).find( "i" ).attr("class");
     showSpinner($(this).find("i"))
     window.location = openTemplateUrl + '?id=' + objectID;
+    hideSpinner($(this).find("i"), icon)
 });
 
 
 $(".open-record-btn").on('click',function() {
     var $registryRow = $(this).closest('tr');
     var objectID = $registryRow.attr("objectid");
+    var icon = $(this).find( "i" ).attr("class");
     showSpinner($(this).find("i"))
     window.location = openRecordUrl + '?id=' + objectID;
+    hideSpinner($(this).find("i"), icon)
 });
 
 $(".open-form-btn").on('click',function() {
     var $registryRow = $(this).closest('tr');
     var objectID = $registryRow.attr("objectid");
+    var icon = $(this).find( "i" ).attr("class");
     showSpinner($(this).find("i"))
     window.location = openFormUrl + '?id=' + objectID;
+    hideSpinner($(this).find("i"), icon)
 });
 
