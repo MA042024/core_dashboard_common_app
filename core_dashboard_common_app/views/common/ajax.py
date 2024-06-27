76b2cabc70d3967bfa5072ff0f19955e6a98e73f
MA042024 <mohammad.abbas@empa.ch>
2024-06-27 16:48:54 +0200

    Update ajax.py

--- core_dashboard_common_app/views/common/ajax.py
+++ core_dashboard_common_app/views/common/ajax.py
@@ -687,11 +687,4 @@ def edit_record(request):
     request.session['data_content'] = data_content
     request.session['data_title'] = data_title
 
-    # Render 'gensel.html' template with data
-    return JsonResponse({
-        'url': '/gensel',
-        'data_id': data_id,
-        'data_content': data_content,
-        'data_title': data_title,
-        'edit': True,
-    })
+    return redirect('/gensel/edit')
